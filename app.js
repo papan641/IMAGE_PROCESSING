1cf7099a789feb0fdcecb7b9c8d249757714fcfb
ROY <roy@gmail.com>
2025-04-05 22:36:29 +0530

    feat: implement Listing model and add testListing endpoint in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,6 +19,19 @@ async function main (){
 
 app.get("/", (req, res)=>{ 
     res.send("hi, i am root");
+});
+
+app.get("/testListing", async(req, res)=>{
+    let sampleListing = new Listing({
+        title: "my new villa",
+        description: "By the beach",
+        price:1200,
+        location:"calangute, goa",
+        country:"India",
+    });
+   await sampleListing.save();
+   console.log("sample was saved");
+   res,send("successful testing");
 });
 
 app.listen(8080, () => {
---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{ 
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        set: (v) => v === ""? "https://www.vecteezy.com/photo/14001155-amazing-beach-landscape-super-wide-panoramic-exotic-travel-background-luxury-travel-idyllic-couple-honeymoon-love-destination-sunny-sea-sand-sky-exotic-resort-coast-palm-lagoon-seascape-banner" :v
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports =Listing;

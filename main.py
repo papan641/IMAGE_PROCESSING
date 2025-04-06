c2bf1186b4830f00b737e1c1c1de98ef28559bd5
ROY <roy@gmail.com>
2025-04-06 12:52:00 +0530

    feat: add MongoDB insertion logic to main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ if __name__ == "__main__":
     print("welcome to pyMongo")
     client = pymongo.MongoClient("mongodb://localhost:27017")
     print(client)
+    db = client['harry']
+    collection = db['mySampleCollectionForHarry']
+    dictionary = {'name': 'harry', 'marks':45}
+    collection.insert_one(dictionary)
+

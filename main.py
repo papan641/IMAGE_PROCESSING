4ddd6bfb5372fe94ef460859f90f5e2b0da14de3
ROY <roy@gmail.com>
2025-04-06 12:40:57 +0530

    feat: update main.py to welcome users and establish MongoDB connection

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")  
+import pymongo
+
+if __name__ == "__main__":
+    print("welcome to pyMongo")
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    print(client)
